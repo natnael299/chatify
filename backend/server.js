96181bc9e1b8af72e4c32d937268db095f0da385
natnael <natibeyene49@gmail.com>
2025-10-17 18:47:14 +0300

    create a src folder for the json package

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
-const express = require("express");
-
+import express from "express";
 const app = express();
+
+app.get("/api/auth/signup", (req, res) => {
+  res.send("Signup endpoint");
+});
+
+app.get("/api/auth/logoin", (req, res) => {
+  res.send("Login Endpoint");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  res.send("Logout Endpoint");
+})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
